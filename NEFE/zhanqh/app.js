6ed4ccd9de1420aa1ad336a29c725952544d7611
scnuzhan <20130800322@m.scnu.edu.cn>
2016-08-31 11:24:05 +0800

    update

--- NEFE/zhanqh/app.js
+++ NEFE/zhanqh/app.js
@@ -1,6 +1,9 @@
 // app.js
 var express = require('express');
+var path = require('path');
 var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // 加载路由模块
 var home = require('./routes/index');
@@ -11,5 +14,7 @@ app.use('/', home);  // 主页
 app.use('/blog', blog);	// 博客
 app.use('/bus', bus); // 公交查询
 app.listen(3030);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 console.log("服务器启动成功，地址：127.0.0.1:3030");
